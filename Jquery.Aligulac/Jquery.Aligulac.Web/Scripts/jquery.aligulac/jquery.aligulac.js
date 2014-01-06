5ade4b3c520d2e0a6c482ac04d6e7d3ae1e1c985
HotspurHN <orb.winter@gmail.com>
2014-01-07 01:16:32 +0300

    aligulactDomParser, popup

--- Jquery.Aligulac/Jquery.Aligulac.Web/Scripts/jquery.aligulac/jquery.aligulac.js
+++ Jquery.Aligulac/Jquery.Aligulac.Web/Scripts/jquery.aligulac/jquery.aligulac.js
@@ -8,45 +8,162 @@ $.fn.aligulac = function (params) {
 	{
 		console.log('Aligulac plugin: Critical error! Mode isn\'t specified!');
 	}
+	params = initDefaultParameters(params);
 	switch(params.mode)
 	{
 		case "player-link-by-id":
-			$.ajax({
-				type: "GET",
-				url: aligulacConfig.aligulacApiRoot + 
-					'player/' + 
-					params.parameters.playerId +
-					'?apikey=' +
-					aligulacConfig.apiKey + '&callback=?',
-					dataType: "json",
-			}).done(function (ajaxData) {
-				var aligulacResult = aligulacMarkup.playerLink;
-				var aligulacFlag = '';
-				if (params.parameters.showFlag) {
-					aligulacFlag = aligulacMarkup.playerFlag
-						.replace('{aligulac-player-flag-image}', aligulacConfig.flagsDirectory + ajaxData.country.toLowerCase() + '.png')
-						.replace('{aligulac-player-flag-name}', ajaxData.country);
-				}
-				var aligulacRace = '';
-				if (params.parameters.showRace) {
-					aligulacRace = aligulacMarkup.playerRace
-						.replace('{aligulac-player-race-image}', aligulacConfig.racesDirectory + ajaxData.race.toUpperCase() + '.png')
-						.replace('{aligulac-player-race-name}', ajaxData.race);
-				}
-				var aligulacClan = '';
-				if (params.parameters.showTeam) {
-					aligulacClan = aligulacMarkup.teamLink
-						.replace('{aligulact-team-url}', aligulacConfig.aligulacRoot + ajaxData.current_teams[0].team.resource_uri.replace('/api/beta/', ''))
-						.replace('{aligulac-team-name}', ajaxData.current_teams[0].team.shortname);
-				}
-
-				aligulacResult = aligulacResult.replace('{flag-image}', aligulacFlag)
-					.replace('{race-image}', aligulacRace)
-					.replace('{team-link}', aligulacClan)
-					.replace('{aligulac-player-link}', aligulacConfig.aligulacRoot + '/players/' + params.parameters.playerId)
-					.replace('{aligulac-player-name}', ajaxData.lp_name);
-
-				domElement.html(domElement.html() + aligulacResult);
-			});
+			playerLinkById(params, domElement);
+			break;
+		case "player-link-by-name":
+			playerLinkByName(params, domElement);
+			break;
 	}
 };
+
+function AligulacParseDomAttributes() {
+	$("[data-aligulac-player-id]").each(function() {
+		$(this).aligulac({
+			mode: 'player-link-by-id',
+			parameters: {
+				playerId: $(this).attr('data-aligulac-player-id')
+	}
+		});
+	});
+	$("[data-aligulac-player-name]").each(function () {
+		$(this).aligulac({
+			mode: 'player-link-by-name',
+			parameters: {
+				playerName: $(this).attr('data-aligulac-player-name')
+			}
+		});
+	});
+}
+
+function playerLink(params, ajaxData, domElement) {
+	var aligulacResult = aligulacMarkup.playerLink;
+
+
+	var aligulacFlag = '';
+	if (params.parameters.showFlag) {
+		aligulacFlag = aligulacMarkup.playerFlag
+			.replace('{aligulac-player-flag-image}', aligulacConfig.flagsDirectory + ajaxData.country.toLowerCase() + '.png')
+			.replace('{aligulac-player-flag-name}', ajaxData.country);
+	}
+	var aligulacRace = '';
+	if (params.parameters.showRace) {
+		aligulacRace = aligulacMarkup.playerRace
+			.replace('{aligulac-player-race-image}', aligulacConfig.racesDirectory + ajaxData.race.toUpperCase() + '.png')
+			.replace('{aligulac-player-race-name}', getFullRaceName(ajaxData.race));
+	}
+	var aligulacClan = '';
+	if (params.parameters.showTeam) {
+		if (ajaxData.current_teams.length > 0) {
+			aligulacClan = aligulacMarkup.teamLink
+				.replace('{aligulact-team-url}', aligulacConfig.aligulacRoot + '/teams/' + ajaxData.current_teams[0].team.id)
+				.replace('{aligulac-team-name}', ajaxData.current_teams[0].team.shortname);
+		}
+	}
+
+	aligulacResult = aligulacResult.replace('{flag-image}', aligulacFlag)
+		.replace('{race-image}', aligulacRace)
+		.replace('{team-link}', aligulacClan)
+		.replace('{aligulac-player-link}', aligulacConfig.aligulacRoot + '/players/' + ajaxData.id)
+		.replace('{aligulac-player-name}', ajaxData.tag);
+
+	domElement.html(domElement.html() + aligulacResult);
+	if (params.parameters.showPopup) {
+		var aligulacPopupContent = '';
+		var teams = '';
+		if (ajaxData.current_teams.length > 0) {
+			teams = ajaxData.current_teams[0].team.shortname;
+		}
+		
+		aligulacPopupContent = aligulacMarkup.playerPopup
+			.replace('{aligulac-player-name}', ajaxData.tag)
+			.replace('{race-image}', aligulacMarkup.playerRace
+				.replace('{aligulac-player-race-image}', aligulacConfig.racesDirectory + ajaxData.race.toUpperCase() + '.png')
+				.replace('{aligulac-player-race-name}', getFullRaceName(ajaxData.race)))
+			.replace('{aligulac-player-race-name}', getFullRaceName(ajaxData.race))
+			.replace('{flag-image}', aligulacMarkup.playerFlag
+				.replace('{aligulac-player-flag-image}', aligulacConfig.flagsDirectory + ajaxData.country.toLowerCase() + '.png')
+				.replace('{aligulac-player-flag-name}', ajaxData.country))
+			.replace('{aligulac-player-flag-name}', ajaxData.country)
+			.replace('{aligulac-player-full-name}', ajaxData.name)
+			.replace('{aligulac-player-akas}', ajaxData.aliases.join(','))
+			.replace('{aligulac-player-birthday}', ajaxData.birthday)
+			.replace('{aligulac-team-name}', teams);
+		domElement.find('.aligulac-player-link').qtip({
+			content: {
+				text: aligulacPopupContent,
+				position: {
+					target: 'mouse', 
+					adjust: { x: 5, y: 5 },
+					style: 'fixed'
+				}
+			}
+		});
+	}
+}
+
+function playerLinkById(params, domElement) {
+	$.ajax({
+		type: "GET",
+		url: aligulacConfig.aligulacApiRoot +
+			'player/' +
+			params.parameters.playerId +
+			'?apikey=' +
+			aligulacConfig.apiKey + '&callback=?',
+		dataType: "json",
+	}).done(function (ajaxData) {
+		playerLink(params, ajaxData, domElement);
+	});
+}
+function playerLinkByName(params, domElement) {
+	$.ajax({
+		type: "GET",
+		url: aligulacConfig.aligulacApiRoot +
+			'player/' +
+			'?apikey=' +
+			aligulacConfig.apiKey +
+			'&tag__iregex=' + params.parameters.playerName +
+			'&callback=?',
+		dataType: "json",
+	}).done(function (ajaxData) {
+		playerLink(params, ajaxData.objects[0], domElement);
+	});
+}
+
+function getFullRaceName(raceSymbol) {
+	var raceName = '';
+	switch (raceSymbol) {
+		case "Z":
+			raceName = "Zerg";
+			break;
+		case "P":
+			raceName = "Protos";
+			break;
+		case "T":
+			raceName = "Terran";
+			break;
+		case "R":
+			raceName = "Random";
+			break;
+	}
+	return raceName;
+}
+
+function initDefaultParameters(params) {
+	if (params.parameters.showFlag == null) {
+		params.parameters.showFlag = true;
+	}
+	if (params.parameters.showRace == null) {
+		params.parameters.showRace = true;
+	}
+	if (params.parameters.showTeam == null) {
+		params.parameters.showTeam = true;
+	}
+	if (params.parameters.showPopup == null) {
+		params.parameters.showPopup = true;
+	}
+	return params;
+}
--- Jquery.Aligulac/Jquery.Aligulac.Web/Scripts/jquery.aligulac/jquery.aligulac.markup.js
+++ Jquery.Aligulac/Jquery.Aligulac.Web/Scripts/jquery.aligulac/jquery.aligulac.markup.js
@@ -3,5 +3,14 @@
 	playerLink: "{flag-image}{race-image}{team-link}<a href='{aligulac-player-link}' class='aligulac-player-link'>{aligulac-player-name}</a>",
 	playerFlag: "<img src='{aligulac-player-flag-image}' alt='{aligulac-player-flag-name}' />",
 	playerRace: "<img src='{aligulac-player-race-image}' alt='{aligulac-player-race-name}' />",
-	teamLink: "<a href='{aligulact-team-url}' class='aligulac-team-link'>{aligulac-team-name}</a>."
+	teamLink: "<a href='{aligulact-team-url}' class='aligulac-team-link'>{aligulac-team-name}</a>.",
+	playerPopup:"<div class='aligulac-popup-content'>" +
+		"<ul><li class='aligulac-popup-header'>{aligulac-player-name}</li>" +
+		"<li><strong>Race:</strong> {race-image} {aligulac-player-race-name}</li>" +
+		"<li><strong>Country:</strong> {flag-image} {aligulac-player-flag-name}</li>" +
+		"<li><strong>Full name:</strong>{aligulac-player-full-name}</li>" +
+		"<li><strong>Aka:</strong>{aligulac-player-akas}</li>" +
+		"<li><strong>Birthday:</strong>{aligulac-player-birthday}</li>" +
+		"<li><strong>Team:</strong>{aligulac-team-name}</li>" +
+		"</ul></div>"
 }
